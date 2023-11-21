b57af6900d118f74ba52855ba0efa57d3699a243
christyuda <chrisyuda@ulbi.ac.id>
2023-11-21 16:00:28 +0700

    feat(surveikepuasandosen):get data survei kepuasan dosen

---
+++ assets/js/survei_kepuasan_dosen.js
@@ -0,0 +1,32 @@
+import { CihuyWithoutToken } from "https://c-craftjs.github.io/simpelbi/api.js";
+
+function populateTable(data) {
+  const tableBody = document.getElementById("tableBody");
+
+  data.forEach((item, index) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+        <td class="py-2 px-4 border-b">${index + 1}</td>
+        <td class="py-2 px-4 border-b">${item.judul}</td>
+        <td class="py-2 px-4 border-b">${item.file}</td>
+
+        <td class="py-2 px-4 border-b">
+          <a href="${
+            item.link_dokumen
+          }" class="text-blue-500 hover:underline" download>Download</a>
+        </td>
+      `;
+    tableBody.appendChild(row);
+  });
+  //
+}
+const apiUrlDataDokumen =
+  "https://simbe-dev.ulbi.ac.id/api/v1/datakepuasandosen/";
+CihuyWithoutToken(apiUrlDataDokumen, (error, data) => {
+  if (error) {
+    console.error("Error fetching data:", error);
+  } else {
+    populateTable(data.data);
+    console.log(data.data);
+  }
+});
