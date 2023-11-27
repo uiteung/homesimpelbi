7fb1143a2c6e1d08c51308a3eb243e190db84bf9
christyuda <chrisyuda@ulbi.ac.id>
2023-11-27 15:35:09 +0700

    feat(beritaindex): menambahkan berita di index

--- assets/js/detailberita.js
+++ assets/js/detailberita.js
@@ -1,30 +1,30 @@
 import { CihuyWithoutToken } from "https://c-craftjs.github.io/simpelbi/api.js";
+function fetchAndDisplayContent() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const id_berita_spmi = urlParams.get("id");
 
-function populateNews(data) {
-  const sortedData = data.sort(
-    (a, b) => new Date(b.tanggal) - new Date(a.tanggal)
-  );
-  const selectedData = sortedData.slice(0, 3);
-  const newsContainer = document.getElementById("newsContainer");
-
-  selectedData.forEach((item) => {
-    const article = document.createElement("div");
-    article.className = "w-33percent flex mb-30 px-15 md:w-full";
-    article.innerHTML = `
-        <article class="lqd-lp relative lqd-lp lqd-lp-style-16 text-start post-${item.id_berita_spmi} post type-post status-publish format-standard has-post-thumbnail hentry category-blog-single tag-beauty">
-          <!-- ... Your news content structure ... -->
-        </article>
-      `;
-    newsContainer.appendChild(article);
-  });
+  // Check if id_berita_spmi is available
+  if (id_berita_spmi) {
+    // Fetch content based on id_berita_spmi using CihuyWithoutToken
+    const apiUrl = `https://simbe-dev.ulbi.ac.id/api/v1/databeritaspmi/${id_berita_spmi}`;
+    CihuyWithoutToken(apiUrl, (error, data) => {
+      if (error) {
+        console.error("Error fetching detail content:", error);
+      } else {
+        // Display content in the #detailContent div
+        const detailContent = document.getElementById("detailContent");
+        detailContent.innerHTML = `
+                    <!-- Modify this section to display the fetched content -->
+                    <h3 class="ld-fh-element relative mb-1em text-24">${data.judul}</h3>
+                    <p class="ld-fh-element relative mb-1em">${data.isi}</p>
+                    <!-- Add other content elements as needed -->
+                `;
+      }
+    });
+  } else {
+    console.error("No id_berita_spmi provided in the URL.");
+  }
 }
 
-// Fetch data from the API
-const apiUrl = "https://simbe-dev.ulbi.ac.id/api/v1/databeritaspmi/";
-CihuyWithoutToken(apiUrl, (error, data) => {
-  if (error) {
-    console.error("Error fetching data:", error);
-  } else {
-    populateNews(data.data);
-  }
-});
+// Call the function when the page loads
+window.onload = fetchAndDisplayContent;
